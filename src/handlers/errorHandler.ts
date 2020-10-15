725e6719f381e87fbab6d1908a2e1a6cad590e1e
Edgar Marques <edgarmarques.110@gmail.com>
2020-10-15 16:02:34 -0300

    Hotfix(ip-track): Add second analytic

--- src/handlers/errorHandler.ts
+++ src/handlers/errorHandler.ts
@@ -3,6 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 export default (error: Error, req: Request, res: Response, next: NextFunction) => {
   res.locals.message = error.message || error;
   res.locals.status = res.locals.status || 400;
+  console.log(error);
   // @ts-ignore
   if (error.isAxiosError) res.locals.status = error.response.status;
   res.locals.isError = true;
